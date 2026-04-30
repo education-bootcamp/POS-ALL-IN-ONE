77b930f916b126ab2a1392c68f91cfdaf5d1873c
education-bootcamp <education.seekerscloud@gmail.com>
2026-04-30 21:05:23 +0530

    2

--- src/main/java/com/devstack/POS/entity/CustomerOrder.java
+++ src/main/java/com/devstack/POS/entity/CustomerOrder.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 @Setter
 @ToString
 @Entity
+@Builder
 @Table(name = "customer_order")
 public class CustomerOrder {
     @Id
--- src/main/java/com/devstack/POS/entity/OrderDetails.java
+++ src/main/java/com/devstack/POS/entity/OrderDetails.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @Setter
 @ToString
 @Entity
+@Builder
 @Table(name = "order_details")
 public class OrderDetails {
     @Id
