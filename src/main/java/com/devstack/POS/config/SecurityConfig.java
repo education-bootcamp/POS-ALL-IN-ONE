d7e5be9bedbf411ea8320d0fd632401b7e1d3480
education-bootcamp <education.seekerscloud@gmail.com>
2026-05-02 20:55:34 +0530

    9

--- src/main/java/com/devstack/POS/config/SecurityConfig.java
+++ src/main/java/com/devstack/POS/config/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.devstack.POS.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
 
     @Bean
-    public PasswordEncorder passwordEncorder(){
-        return new BCryptPa
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
+++ src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
@@ -3,17 +3,20 @@ package com.devstack.POS.service.impl;
 import com.devstack.POS.dto.request.LoginRequestDTO;
 import com.devstack.POS.dto.request.RegisterRequestDTO;
 import com.devstack.POS.dto.response.AuthResponseDTO;
+import com.devstack.POS.entity.ROLE_TYPES;
 import com.devstack.POS.entity.SystemUser;
 import com.devstack.POS.exception.DuplicateEntryException;
 import com.devstack.POS.repo.SystemUserRepo;
 import com.devstack.POS.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final SystemUserRepo systemUserRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void register(RegisterRequestDTO dto) {
@@ -24,11 +27,12 @@ public class AuthServiceImpl implements AuthService {
         SystemUser user = SystemUser.builder()
                 .fullName(dto.getFullName())
                 .email(dto.getEmail())
-                .password(dto.getPassword()) //wrong
-                .role()
-                .isActive()
-                .build()
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .role(ROLE_TYPES.USER)
+                .isActive(true)
                 .build();
+
+        systemUserRepo.save(user);
 
     }
 
