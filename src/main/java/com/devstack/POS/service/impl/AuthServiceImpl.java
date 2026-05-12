4a4531e48dc2044484f89cf53a525b099c527b06
education-bootcamp <education.seekerscloud@gmail.com>
2026-05-12 20:42:33 +0530

    18

--- src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
+++ src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
@@ -8,15 +8,22 @@ import com.devstack.POS.entity.SystemUser;
 import com.devstack.POS.exception.DuplicateEntryException;
 import com.devstack.POS.repo.SystemUserRepo;
 import com.devstack.POS.service.AuthService;
+import com.devstack.POS.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final SystemUserRepo systemUserRepo;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @Override
     public void register(RegisterRequestDTO dto) {
@@ -38,6 +45,21 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthResponseDTO login(LoginRequestDTO dto) {
-        authenticationManager.authenticate
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getEmail(), dto.getPassword()
+                )
+        );
+        SystemUser systemUser = systemUserRepo.findSystemUserByEmail(dto.getEmail())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        String token = jwtUtil.generateAccessToken(systemUser);
+        return AuthResponseDTO.builder()
+                .role(systemUser.getRole().name())
+                .token(token)
+                .tokenType("Bearer")
+                .fullName(systemUser.getFullName())
+                .email(systemUser.getEmail())
+                .build();
     }
 }
